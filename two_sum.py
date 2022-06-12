dff8456feaa9b9861e7ef95d7c8161746c6651e6
bird.ron <bird.ron@pm.me>
2022-06-12 16:23:32 -0700

    Implemented two sum.

---
+++ two_sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    # for the first number, we don't need to check for a solution
+    # however, we do need to remember its index
+    #
+    # for a subsequent number x, the number y we need for a solution is equal to the target minus x
+    # if we have seen y before, return its index and the current index
+    # otherwise, remember the index of the current number
+    @staticmethod
+    def twoSum(nums: list[int], target: int) -> tuple[int, int]:
+        num_to_idx: dict[int, int] = {nums[0]: 0}
+        i: int
+        num: int
+        for i, num in enumerate(nums[1:], 1):
+            partner: int = target - num
+            if partner in num_to_idx:
+                return i, num_to_idx[partner]
+            num_to_idx[num] = i
