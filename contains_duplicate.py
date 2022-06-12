d26ece74d103f60f4f311d9f9fbe95491e5bb658
bird.ron <bird.ron@pm.me>
2022-06-12 12:37:57 -0700

    Added a comment.

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -1,5 +1,6 @@
 # noinspection PyPep8Naming
 class Solution:
+    # if the set of numbers has fewer elements than the sequence of numbers, then a duplicate exists
     @staticmethod
     def containsDuplicate(nums: list[int]) -> bool:
         return len(set(nums)) < len(nums)
