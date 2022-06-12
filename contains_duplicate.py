515c0d74882436fbe75d7d79ff210186b36cfeeb
bird.ron <bird.ron@pm.me>
2022-06-12 13:04:15 -0700

    Add some other solutions.

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -4,3 +4,25 @@ class Solution:
     @staticmethod
     def containsDuplicate(nums: list[int]) -> bool:
         return len(set(nums)) < len(nums)
+
+    # if the set already contains the number, then a duplicate exists
+    @staticmethod
+    def setContainsDuplicate(nums: list[int]) -> bool:
+        s: set[int] = {nums[0]}
+        n: int
+        for n in nums[1:]:
+            if n in s:
+                return True
+            s.add(n)
+        return False
+
+    # in a sorted sequence of numbers, duplicates will be neighbors
+    @staticmethod
+    def sortContainsDuplicate(nums: list[int]) -> bool:
+        nums.sort()
+        n1: int
+        n2: int
+        for n1, n2 in zip(nums, nums[1:]):
+            if n1 == n2:
+                return True
+        return False
