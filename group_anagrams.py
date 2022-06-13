0fa855892a3accfa7943360238f4a16988bb64b6
bird.ron <bird.ron@pm.me>
2022-06-12 18:57:30 -0700

    Implemented Group Anagrams.

---
+++ group_anagrams.py
@@ -0,0 +1,53 @@
+from itertools import groupby
+
+
+# create a sequence containing a zero for every lowercase letter of the english alphabet
+# increment this number every time we see that letter
+# create a comma-separated string of these values to use as a key
+def string_key(string: str) -> str:
+    char_counts: list[int] = [0] * 26
+    char: str
+    for char in string:
+        char_counts[ord(char) - ord("a")] += 1
+    return ",".join(str(char_count) for char_count in char_counts)
+
+
+# noinspection PyTypeChecker
+# py type checker is saying that this should return frozenset[str]
+# which it never does
+#
+# create a mapping from character to integer
+# this integer begins at zero and is incremented every time we see the corresponding character
+# the mapping is implemented with a dictionary, which is unhashable because it is mutable
+# to fix this, convert the items in the dictionary to a frozen set, which is hashable because it is immutable
+def cold_key(string: str) -> frozenset[tuple[str, int]]:
+    char_counts: dict[str, int] = {}
+    char: str
+    for char in string:
+        char_counts[char] = char_counts.get(char, 0) + 1
+    return frozenset(char_counts.items())
+
+
+# noinspection PyPep8Naming
+class Solution:
+    # create a mapping from string => key => anagrams
+    # the key must be the same for anagrams, and must be hashable
+    # return a list of lists of anagrams
+    @staticmethod
+    def groupAnagrams(strings: list[str]) -> list[list[str]]:
+        key_to_group: dict[frozenset[tuple[str, int]], list[str]] = {}
+        string: str
+        for string in strings:
+            key: frozenset[tuple[str, int]] = cold_key(string)
+            key_to_group[key] = key_to_group.get(key, [])
+            key_to_group[key].append(string)
+        return list(key_to_group.values())
+
+    # sort the strings by their sorted representation
+    # this sorts the anagrams into contiguous groups
+    # put every group into its own list
+    @staticmethod
+    def oneLinerGroupAnagrams(strings: list[str]) -> list[list[str]]:
+        # mypy complains about the groupby call due to a bug
+        # https://github.com/python/mypy/issues/9920
+        return [list(group) for key, group in groupby(sorted(strings, key=sorted), sorted)]  # type: ignore
