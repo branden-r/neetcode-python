cf13ec403c5b1c70cd0453f82f6a187025c75092
bird.ron <bird.ron@pm.me>
2022-06-13 20:08:22 -0700

    Added a second implementation that doesn't import any collections. Changed some names to be clearer.

--- group_anagrams.py
+++ group_anagrams.py
@@ -10,8 +10,26 @@ class Solution:
     # two strings are anagrams iff they have the same number of each character
     @staticmethod
     def groupAnagrams(strings: list[str]) -> list[list[str]]:
-        anagram_to_group: dict[frozenset[tuple[str, int]], list[str]] = defaultdict(list)
+        groups: dict[frozenset[tuple[str, int]], list[str]] = defaultdict(list)
         string: str
         for string in strings:
-            anagram_to_group[frozenset(Counter(string).items())].append(string)
-        return list(anagram_to_group.values())
+            groups[frozenset(Counter(string).items())].append(string)
+        return list(groups.values())
+
+    # same as above, but without importing collections
+    @staticmethod
+    def noCollectionsGroupAnagrams(strings: list[str]) -> list[list[str]]:
+        groups: dict[frozenset[tuple[str, int]], list[str]] = {}
+        string: str
+        for string in strings:
+            counter: dict[str, int] = {}
+            char: str
+            for char in string:
+                counter[char] = counter.get(char, 0) + 1
+            # noinspection PyTypeChecker
+            # py type checker thinks this is a frozen set of strings
+            # it isn't
+            key: frozenset[tuple[str, int]] = frozenset(counter.items())
+            groups[key] = groups.get(key, [])
+            groups[key].append(string)
+        return list(groups.values())
